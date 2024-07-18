acf2ab3b79d65f28c656c87419beeda0526e1ee8
Ken Baum <ilpadre1953@gmail.com>
2024-07-18 08:49:06 -0400

    Code from C3

---
+++ Solutions/KenSolutions/CSharp/NaturalSortingC3/NaturalSortingC3.Test/UnitTest1.cs
@@ -0,0 +1,117 @@
+namespace NaturalSortingC3.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void ParseStringHandlesEmptyString()
+        {
+            string input = "";
+            var actual = Program.ParseString(input);
+            Assert.That(actual, Is.EqualTo(null));
+        }
+
+        [Test]
+        public void ParseStringParsesCharsOnly()
+        {
+            string input = "abc";
+            var actual = Program.ParseString(input);
+            Assert.That(actual.LeadingChars, Is.EqualTo("abc"));
+            Assert.That(actual.Number, Is.EqualTo(-1));
+            Assert.That(actual.TrailingChars, Is.EqualTo(""));
+        }
+
+        [Test]
+        public void ParseStringParsesDigitsOnly()
+        {
+            string input = "123";
+            var actual = Program.ParseString(input);
+            Assert.That(actual.LeadingChars, Is.EqualTo(""));
+            Assert.That(actual.Number, Is.EqualTo(123));
+            Assert.That(actual.TrailingChars, Is.EqualTo(""));
+        }
+
+        [Test]
+        public void ParseStringParsesLeadingDigits()
+        {
+            string input = "123abc";
+            var actual = Program.ParseString(input);
+            Assert.That(actual.LeadingChars, Is.EqualTo(""));
+            Assert.That(actual.Number, Is.EqualTo(123));
+            Assert.That(actual.TrailingChars, Is.EqualTo("abc"));
+        }
+
+        [Test]
+        public void ParseStringParsesFirstWord()
+        {
+            string input = "abc123efg";
+            var actual = Program.ParseString(input);
+            Assert.That(actual.LeadingChars, Is.EqualTo("abc"));
+            Assert.That(actual.Number, Is.EqualTo(123));
+            Assert.That(actual.TrailingChars, Is.EqualTo("efg"));
+        }
+
+        [Test]
+        public void ParseStringParsesFirstWordNumberOnly()
+        {
+            string input = "abc123";
+            var actual = Program.ParseString(input);
+            Assert.That(actual.LeadingChars, Is.EqualTo("abc"));
+            Assert.That(actual.Number, Is.EqualTo(123));
+            Assert.That(actual.TrailingChars, Is.EqualTo(""));
+        }
+
+        [Test]
+        public void ParseStringParsesFirstWordNumberAndStuff()
+        {
+            string input = "file1bblah234";
+            var actual = Program.ParseString(input);
+            Assert.That(actual.LeadingChars, Is.EqualTo("file"));
+            Assert.That(actual.Number, Is.EqualTo(1));
+            Assert.That(actual.TrailingChars, Is.EqualTo("bblah234"));
+
+            var actual2 = Program.ParseString(actual.TrailingChars);
+            Assert.That(actual2.LeadingChars, Is.EqualTo("bblah"));
+            Assert.That(actual2.Number, Is.EqualTo(234));
+            Assert.That(actual2.TrailingChars, Is.EqualTo(""));
+
+            var actual3 = Program.ParseString(actual2.TrailingChars);
+            Assert.That(actual3, Is.Null); 
+        }
+
+        [Test]
+        public void NaturalSortAscending_Test1()
+        {
+            string[] unsorted = { "file10", "file2", "file1b-blah234", "file1a", "file1b", "file20", "file11", "file3" };
+            string[] sortedTarget = { "file1a", "file1b", "file1b-blah234", "file2", "file3", "file10", "file11", "file20" };
+            string[] sortedActual = Program.SortNaturally(unsorted);
+            Assert.IsTrue(sortedTarget.SequenceEqual(sortedActual));
+        }
+
+        [Test]
+        public void NaturalSortAscending_Test2()
+        {
+            string[] unsorted = { "file10", "file2", "file1b-blah234", "file1a", "file1b", "123", "file20", "file11", "123abc", "file3" };
+            string[] sortedTarget = { "123", "123abc", "file1a", "file1b", "file1b-blah234", "file2", "file3", "file10", "file11", "file20" };
+            string[] sortedActual = Program.SortNaturally(unsorted);
+            Assert.IsTrue(sortedTarget.SequenceEqual(sortedActual));
+        }
+
+        [Test]
+        public void CompareNaturally()
+        {
+            string input = "file1bblah234";
+            string input2 = "file1bblah234a";
+            var actual = Program.CompareStringsNaturally(input, input2);
+            Assert.That(actual, Is.EqualTo(-1));
+
+            var actual2 = Program.CompareStringsNaturally(input2, input);
+            Assert.That(actual2, Is.EqualTo(1));
+
+        }
+    }
+}
---
+++ Solutions/KenSolutions/CSharp/NaturalSortingC3/NaturalSortingC3/Program.cs
@@ -0,0 +1,85 @@
+﻿using System.Text.RegularExpressions;
+
+namespace NaturalSortingC3
+{
+    public class Program
+    {
+        public class ParseResult
+        {
+           public string? LeadingChars { get; set; }
+           public int Number { get; set; }
+           public string? TrailingChars { get; set; }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public static string[] SortNaturally(string[] unsorted)
+        {
+            string[] sorted = SortUnsorted(unsorted);
+            return sorted;
+        }
+
+        private static string[] SortUnsorted(string[] unsorted)
+        {
+            var sortedArray = unsorted;
+            Array.Sort(sortedArray, CompareStringsNaturally);
+            return sortedArray;
+        }
+
+        public static ParseResult? ParseString(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return null;
+
+            string pattern = @"^(\D*)(\d*)(.*)";
+            Match match = Regex.Match(input, pattern);
+            ParseResult pr = new ParseResult();
+            pr.LeadingChars = match.Groups[1].Value;
+            pr.Number = string.IsNullOrEmpty(match.Groups[2].Value) ? -1: int.Parse(match.Groups[2].Value);
+            pr.TrailingChars = string.IsNullOrEmpty(match.Groups[3].Value) ? "" : match.Groups[3].Value;
+
+            return pr;
+
+        }
+
+
+        public static int CompareStringsNaturally(string? a, string? b)
+        {
+            // if a < b return -1
+            // if a == b return 0
+            // if a > b return 1
+            var aLeadingChars = ParseString(a);
+            var bLeadingChars = ParseString(b);
+
+            if (aLeadingChars == null && bLeadingChars == null)
+                return 0;
+
+            if (aLeadingChars == null)
+            {
+                return -1;
+            }
+
+            if (bLeadingChars == null)
+            {
+                return 1;
+            }
+
+            if (aLeadingChars.LeadingChars != bLeadingChars.LeadingChars)
+            {
+                return string.Compare(aLeadingChars.LeadingChars, bLeadingChars.LeadingChars);
+            }
+
+            if (aLeadingChars.Number != bLeadingChars.Number)
+            {
+                return aLeadingChars.Number.CompareTo(bLeadingChars.Number);
+            }
+
+            return CompareStringsNaturally(aLeadingChars.TrailingChars, bLeadingChars.TrailingChars);
+
+        }
+
+    }
+}
